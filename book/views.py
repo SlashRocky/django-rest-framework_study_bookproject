8f807bec76f24f78c8ee113b253c94d197c48ae3
SlashRocky <xxx.good.luck.my.love.xxx@gmail.com>
2021-04-07 15:40:59 +0900

    create simple API server

---
+++ book/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from rest_framework.generics import ListCreateAPIView
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
+
+
+# Create your views here.
+class BookList(ListView):
+    template_name = 'list.html'
+    model = Book
+
+
+class BookListAPI(ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
